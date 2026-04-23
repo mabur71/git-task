36e7bada40b2e49d835d1769d29b183157e7b3c2
Максим Бурков <mabur71@ya.ru>
2026-04-23 12:30:28 +0500

    update calc

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
-# Автор: ...
+# Автор: Максим Бурков
 
+import math
 def add(a, b):
     return a + b
 
@@ -10,7 +11,10 @@ def subtract(a, b):
 
 def multiply(a, b):
     # TODO: не реализовано
-    return None
+    return a*b
+
+def sqrt(x):
+    return math.sqrt(x)
 
 
 if __name__ == "__main__":
